a87aa87cc0b9e3a401d7b274363b039be1e1c697
hf73 <70991020+hf73@users.noreply.github.com>
2022-11-06 12:16:15 -0500

    added method to create new user and redirect to landing page

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash #helps to hash passwords
 
 auth = Blueprint('auth', __name__)
 
@@ -20,14 +22,23 @@ def register():
         username = request.form.get('username')
         password = request.form.get('password')
 
+        #checks length of email and returns error
         if len(email) < 5:
             flash('Email must be greater than 5 characters', category='error')
+        #checks length of username and returns error
         elif len(username) < 3:
             flash('Username must be greater than 3 characters', category='error')
+        #checks length of password and returns error    
         elif len(password) < 6:
             flash('Password must be greater than 6 characters', category='error')
         else:
-            #add user to database
+            #add user to database. hash password as sha256 hashing algorithm
+            new_user = User(email=email, username=username, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user) #adds user to the database
+            db.session.commit() #commits the user to the database
             flash('Successfully created account!', category='success')
+           
+            #redirects to landing page (home page)
+            return redirect(url_for('views.home')) 
 
     return render_template("register.html")
