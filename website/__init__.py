b6307bbd01668bd13c46dfcdfb2763e14b90742d
ChrisMiranda1178 <chrismiranda331@gmail.com>
2022-11-06 15:14:53 -0500

    Updated Database changes part 2 still error

--- website/__init__.py
+++ website/__init__.py
@@ -33,8 +33,8 @@ def create_app():
 
     return app
 
-
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+        with app.app_context():  
+            db.create_all(app=app)
+            print('Created Database!')
