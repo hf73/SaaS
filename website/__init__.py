2c4f4c250413c801ff8f511281beb6503ea7b8fa
ChrisMiranda1178 <43075644+ChrisMiranda1178@users.noreply.github.com>
2022-11-21 21:16:24 -0500

    Update __init__.py
    "Now with notes because fuck my job and the boss that works here"

--- website/__init__.py
+++ website/__init__.py
@@ -6,14 +6,14 @@ from website import models
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
-DB2_NAME = "database2.db"
+DB2_NAME = "database2.db" #Named Database 2 to avoid confusions
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'key'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['SQLALCHEMY_BINDS']={'two' : f'sqlite:////{DB2_NAME}'}
+    app.config['SQLALCHEMY_BINDS']={'two' : f'sqlite:////{DB2_NAME}'} #Using SQLAlchemy Binds allows for multiple databases to communicate and copy each others information
     db.init_app(app)
 
     from .views import views
@@ -36,16 +36,16 @@ def create_app():
 
     return app
 
-class One(db.Model):
+class One(db.Model): #Creates the First DataBase
     id= db.Column(db.Integer, primary_key=True)
     
-class Two(db.Model):
+class Two(db.Model):#Creates the Second Database
     __bind_key__ = 'two'
     id= db.Column(db.Integer, primary_key=True)
 
 
 
-def create_database(Model):
+def create_database(Model):#fills all information needed from auth file and creates both Databases
     if not path.exists('website/' + DB_NAME + DB2_NAME):
         with Model.app_context():  
             db.create_all(app=Model)
