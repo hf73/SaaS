675f5a5fc6182800ddf9942fbf07aa1f751a6570
Dannys99 <90282205+Dannys99@users.noreply.github.com>
2022-11-29 21:06:34 -0500

    Connecting rabbitmq

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import pika, sys, os
+credentials = pika.PlainCredentials(username='danny', password='123', erase_on_connect=True)
+parameters = pika.ConnectionParameters(host='10.147.18.152', port=5672, virtual_host='490', credentials=credentials)
+connection = pika.BlockingConnection(parameters)
 from website import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+connection.close()
 
     
