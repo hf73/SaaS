8196f558f45bcb8f76f1fad7f1ee120c50602bad
Dannys99 <90282205+Dannys99@users.noreply.github.com>
2022-12-18 00:10:18 -0500

    Adding rabbitmq connection

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import pika, sys, os
+credentials = pika.PlainCredentials(username='john', password='123', erase_on_connect=True)
+parameters = pika.ConnectionParameters(host='10.147.18.198', port=5672, virtual_host='last', credentials=credentials)
+connection = pika.BlockingConnection(parameters)
 from website import create_app #accesses website file to import create_app from __init__
 
 app = create_app()
@@ -5,4 +9,4 @@ app = create_app()
 if __name__ == '__main__': #if file is ran, not when it is imported, then the website gets ran 
     app.run(debug=True) #anytime a change is done to the code, then the website get rerun (turn off for prod good for dev)
 
-    
+    connection.close()
