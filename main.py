893715dc8bfb337750bcc212c44f713d311a4ce9
hf73 <hf73@njit.edu>
2022-12-19 21:36:22 -0500

    update main for publishing

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from website import create_app #accesses website file to import create_app from 
 app = create_app()
 
 if __name__ == '__main__': #if file is ran, not when it is imported, then the website gets ran 
-    app.run(debug=True) #anytime a change is done to the code, then the website get rerun (turn off for prod good for dev)
+    app.run(debug=True, host='0.0.0.0') #anytime a change is done to the code, then the website get rerun (turn off for prod good for dev)
 
     connection.close()
